99cb58c52e033dd12787d9a7185a314a40b7c9e8
Pham <tpham75@vdc.csc.com>
2017-08-23 11:25:42 +0700

    Version2

--- getlog.sh
+++ getlog.sh
@@ -9,23 +9,27 @@ elif [[ "$M" < "30" ]] ; then M=15
 elif [[ "$M" < "45" ]] ; then M=30
 else M=45
 fi
-TimeStamp="$Y.$m.$d-$H:$M"
+TimeStamp="$Y$m$d-$H:$M"
+LogDate="$Y$m$d"
 
-#copy logs
-
-Pattern1=$1
-Pattern2=$2
-
-echo $Pattern1;
-echo $Pattern2;
-echo $TimeStamp;
-
-if [ "$Pattern2" != "" ]
-then
-    echo "If have PATTERN2"
-	cp $(docker inspect $(docker ps | grep $Pattern1 | awk '{print $1}') | grep 'LogPath' | sed -e 's/,//g' -e 's/"//g' -e 's/LogPath: //g') /home/logs/$Pattern1-$TimeStamp.log
-	LogFile=cat /home/logs/$Pattern1-$TimeStamp.log
-	Newfile=$LogFile | grep $Pattern2 > /home/logs/$Pattern1-$Pattern2.log
-else
-    cp $(docker inspect $(docker ps | grep $Pattern1 | awk '{print $1}') | grep 'LogPath' | sed -e 's/,//g' -e 's/"//g' -e 's/LogPath: //g') /home/logs/$Pattern1-$TimeStamp.log
+#create folder for date
+if [ ! -d "/home/logs/$LogDate" ]; then
+        cd /home/logs
+        mkdir $LogDate
+        echo "Created new logs dir: $LogDate"
 fi
+
+#make file list
+docker ps --format "table {{.ID}}-{{.Names}}" | sed '1d' > "/home/logs/$LogDate/data.txt"
+file="/home/logs/$LogDate/data.txt"
+while IFS='-' read -r col1 col2
+do
+# display $line or do somthing with $line
+cp $(docker inspect $col1 | grep 'LogPath' | sed -e 's/,//g' -e 's/"//g' -e 's/LogPath: //g') /home/logs/$LogDate/$col2-$TimeStamp.log;
+echo "$col1 - $col2";
+done <"$file"
+
+cd /home/logs/$LogDate
+zip -r "log-$TimeStamp.zip" *
+rm *.log
+ls -l
